e9414b4a07158e65e994bdc48c78522cfc554984
detachedsoul <vindicatedwisdom@gmail.com>
2021-05-26 02:31:44 +0100

    Created the sign up / sign in page, added horizontal scroll functionality to featured items in homepage and other minor fixes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-let navToggle   =   document.querySelector('.nav-toggle-btn');
-let navContent  =   document.querySelector('.nav-content');
-let navLinks = document.querySelectorAll('.nav-links');
-
-let searchToggleBtn     =   document.querySelector('.search-toggle');
-let searchContent       =   document.querySelector('.search-container');
-
-let toTopBtn = document.querySelector('.to-top');
-
 window.onscroll = () => {
     if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
         toTopBtn.classList.add('show-to-top');
@@ -15,15 +6,23 @@ window.onscroll = () => {
     }
 };
 
+
+let searchToggleBtn     =   document.querySelector('.search-toggle');
+let searchContent       =   document.querySelector('.search-container');
 searchToggleBtn.addEventListener('click', () => {
     searchContent.classList.toggle('show-search-container');
 });
 
+
+let navToggle   =   document.querySelector('.nav-toggle-btn');
+let navContent  =   document.querySelector('.nav-content');
 navToggle.addEventListener('click', () => {
     navContent.classList.toggle('nav-content-show');
     navToggle.classList.toggle('nav-toggle-close');
 });
 
+
+let navLinks = document.querySelectorAll('.nav-links');
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
         navContent.classList.remove('nav-content-show');
@@ -31,6 +30,7 @@ navLinks.forEach(navLink => {
     });
 });
 
+let toTopBtn = document.querySelector('.to-top');
 toTopBtn.addEventListener('click', () => {
     scrollTo({
         top: 0,
@@ -38,3 +38,40 @@ toTopBtn.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+let container = document.querySelector('.what-you-like-grid');
+let totalHorizontalSlide = document.querySelectorAll('.what-you-like-card').length;
+let counter = 1;
+
+let scrollRightBtn = document.querySelector('.what-you-like-btn.right');
+scrollRightBtn.addEventListener('click', () => {
+    sideScroll(container, 'right', 200);
+});
+
+let scrollLeftBtn = document.querySelector('.what-you-like-btn.left');
+scrollLeftBtn.addEventListener('click', () => {
+    sideScroll(container,'left', 200);
+});
+
+
+function sideScroll (element, direction, speed) {
+    let slideTimer = setTimeout(function () {
+
+        if (direction === 'left') {
+            element.scrollLeft -= element.clientWidth;
+        } else {
+            element.scrollLeft += element.clientWidth;
+        }
+
+        window.clearInterval(slideTimer);
+
+    }, speed);
+}
+
+function sideScrollAuto (element, speed) {
+    let slideTimer = setInterval(function () {
+        element.scrollLeft += element.clientWidth;
+    }, speed);
+}
+
+sideScrollAuto(container, 8000);
